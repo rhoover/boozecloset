9d615d2865218f26bb47baedee21785a8588a9b9
robinHoover <robin.hoover@gmail.com>
2014-09-12 08:26:47 -0400

    POST Fix Attempt

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,12 +22,12 @@ angular.module('boozeApp', ['ngAnimate',  'ngRoute', 'ngTouch'])
 
     })
 
-    .config(function ($routeProvider, $httpProvider) {
+    .config(function ($routeProvider) {
 
         //courtesy: https://gist.github.com/s9tpepper/3328010
         // The PHP $_POST expects data w/ a form content type, not a JSON payload that Angular delivers
-        $httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
-        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+        // $httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+        // $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 
         var booze = function(resolveFactory) {
             return resolveFactory.boozeResolve();
@@ -64,4 +64,50 @@ angular.module('boozeApp', ['ngAnimate',  'ngRoute', 'ngTouch'])
             .otherwise({
                 redirectTo: '/'
         });
+    })
+
+    .config(function ($httpProvider) {
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+        /**
+        * The workhorse; converts an object to x-www-form-urlencoded serialization.
+        * @param {Object} obj
+        * @return {String}
+        * http://victorblog.com/2012/12/20/make-angularjs-http-service-behave-like-jquery-ajax/
+        */
+        var param = function(obj) {
+        var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+
+        for(name in obj) {
+          value = obj[name];
+
+          if(value instanceof Array) {
+            for(i=0; i<value.length; ++i) {
+              subValue = value[i];
+              fullSubName = name + '[' + i + ']';
+              innerObj = {};
+              innerObj[fullSubName] = subValue;
+              query += param(innerObj) + '&';
+            }
+          }
+          else if(value instanceof Object) {
+            for(subName in value) {
+              subValue = value[subName];
+              fullSubName = name + '[' + subName + ']';
+              innerObj = {};
+              innerObj[fullSubName] = subValue;
+              query += param(innerObj) + '&';
+            }
+          }
+          else if(value !== undefined && value !== null)
+            query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+        }
+
+        return query.length ? query.substr(0, query.length - 1) : query;
+        };
+
+        // Override $http service's default transformRequest
+        $httpProvider.defaults.transformRequest = [function(data) {
+        return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+        }];
     });
